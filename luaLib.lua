a7a3710a64dd4c7d1fd97783ea8fbd3a0ae8a94c
Garoze <marcelogaroze@hotmail.com>
2020-11-15 13:46:05 -0300

    remove: olds files and merge the lua.array on the luaLib file

--- luaLib.lua
+++ luaLib.lua
@@ -1,4 +1,5 @@
 local lua = {}
+lua.utils = {}
 
 function lua.print(toPrint)
     if type(toPrint) == 'table' then 
@@ -35,4 +36,48 @@ function lua.filter(array, callFunction)
     end
     return filterResult
 end
+
+function lua.utils.arrayType(arrType, arrElements)
+    assert(type(arrType) == 'string', '[lua.utils.arrayType] - Expected a \'string\' on #1 argument and got '..type(arrType))
+    assert(type(arrElements) == 'table', '[lua.utils.arrayType] - Expected a \'table\' on #2 argument and got '..type(arrElements))
+    for i, element in ipairs(arrElements) do 
+        assert(type(element) == arrType, '[lua.utils.arrayType] - Invalid type on index: '..i..' Expected '..arrType..' got '..type(element))
+    end
+    return arrElements
+end
+
+function lua.array(arrType, arrElements)
+    return setmetatable({
+        __type = 'arrayObject',
+        __arrType = arrType,
+        array = lua.utils.arrayType(arrType, arrElements),
+
+        --> arrayFunctions <--
+        push = function(self, value)
+            assert(type(value) == self.__arrType, '[lua.array -> push] - Expected a \''..arrType..'\' on #1 argument and got \''..type(value)..'\'')
+            table.insert(self.array, value)
+        end,
+
+        pop = function(self)
+            assert(#self.array > 1, '[lua.array -> pop] - Cannot remove the first element of an array')
+            table.remove(self.array, #self.array)
+        end,
+        
+        printFirst = function(self)
+            assert(self.array[1] ~= nil, '[lua.array -> printFirst] - Theres no first valid element to print')
+            print(self.array[1])
+        end,
+
+        printLast = function(self)
+            assert(self.array[#self.array] ~= nil, '[lua.array -> printLast] - Theres no last valid element to print')
+            print(self.array[#self.array])
+        end,
+
+        getArrayLength = function(self)
+            assert(#self.array >= 1, '[lua.array -> getArrayLength] - Theres no valid length for this array')
+            return #self.array
+        end,
+    },{ __call = function(self) return self.array end })
+end
+
 
